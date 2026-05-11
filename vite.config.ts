84c68ea375ae0bd19199f841157a6c006e1f1df5
Milton Fernandies <mm1502061@gmail.com>
2026-05-11 23:56:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    // ADD THIS LINE BELOW
+    allowedHosts: 'all', 
     hmr: {
       overlay: false,
     },
